0f56448d9ce547fece184a861e689e9a020439b1
Markus <markholden1989@googlemail.com>
2018-12-23 17:13:04 +0000

    Add Mode Button Styling

--- src/Recipes/SearchModes.js
+++ src/Recipes/SearchModes.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './SearchModes.css';
 
 class SearchModes extends Component {
 
@@ -29,7 +30,7 @@ class SearchModes extends Component {
         })
 
         return (
-            <ul>
+            <ul className="modes">
                 {modeItems}
             </ul>
         )
