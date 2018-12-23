4b86f33187de2d047bcc1bc8c77561c3aead14e5
Markus <markholden1989@googlemail.com>
2018-12-23 17:04:44 +0000

    Add Exact Match functionality

--- src/Recipes/RecipeMain.js
+++ src/Recipes/RecipeMain.js
@@ -10,6 +10,13 @@ class RecipeMain extends Component {
             if (this.props.search_mode === 'all') {
                 return <Recipe recipe={r}></Recipe>
             } else if (this.props.search_mode === 'exact') {
+                if ((() => {
+                    return r.ingredients.every((l) => {
+                        return this.props.ingredients.indexOf(l) >= 0;
+                    });
+                })()) {
+                    return <Recipe recipe={r}></Recipe>
+                };
             } else if (this.props.search_mode === 'fuzzy') {
                 if ((() => {
                     return this.props.ingredients.some(function (v) {
@@ -19,7 +26,6 @@ class RecipeMain extends Component {
                     return <Recipe recipe={r}></Recipe>
                 }
             } else {
-                // Return all as default
                 return <Recipe recipe={r}></Recipe>
             }
         });
